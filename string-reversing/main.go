adb3da22ee89f4278360607bda5844b7af1c689b
pedro.mendes <pedro.mendes@corp.globo.com>
2018-05-10 09:47:39 -0300

    string-reversing/main: remove solution func implementation from main

--- string-reversing/main.go
+++ string-reversing/main.go
@@ -5,21 +5,6 @@ import (
 	"os"
 )
 
-// Solution for the String Reversing problem. This
-// solution only iterates through half of the string to get its reversed
-func Solution(val []byte) []byte {
-
-	valLength := len(val)
-	halfLength := valLength / 2
-
-	for i := 0;  i < halfLength; i++ {
-		temp := (val)[i]
-
-		(val)[i] = (val)[valLength-1-i]
-		(val)[valLength-1-i] = temp
-	}
-	return val
-}
 
 func main() {
 
