e32513fa0075f6950adf1f46c87a97f46f0ae8b5
pedro.mendes <pedro.mendes@corp.globo.com>
2018-07-18 13:13:58 -0300

    classic/stack/solution: Add isEmpty implementation and tests

--- classic/stack/solution.go
+++ classic/stack/solution.go
@@ -8,26 +8,31 @@ type Node struct {
 
 // Stack defines the basic tree structure
 type Stack struct {
-	root *Node
+	first *Node
 }
 
 // Push add a node to beginning of the Stack
 func (s *Stack) Push(node *Node) {
-	// TODO implement and test
+
 }
 
 // Pop remove a node of beginning of the Stack
-func (s *Stack) Pop(node *Node) {
+func (s *Stack) Pop() (node *Node) {
 	// TODO implement and test
+	return
 }
 
 // Peek return the reference from the Node at the top
 // of the stack
-func (s *Stack) Peek(node *Node) {
+func (s *Stack) Peek() (node *Node) {
 	// TODO implement and test
+	return
 }
 
 // IsEmpty confirms if the stack is empty
-func (s *Stack) IsEmpty(node *Node) {
-	// TODO implement and test
+func (s *Stack) IsEmpty() bool {
+	if s.first == nil {
+		return true
+	}
+	return false
 }
--- classic/stack/solution_test.go
+++ classic/stack/solution_test.go
@@ -1,2 +1,16 @@
 package main
 
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsEmptyAtAnEmptyStack(test *testing.T) {
+	stack := Stack{}
+	assert.True(test, stack.IsEmpty())
+}
+
+func TestIsEmptyAtANonEmptyStack(test *testing.T) {
+	stack := Stack{ first: &Node{} }
+	assert.False(test, stack.IsEmpty())
+}
