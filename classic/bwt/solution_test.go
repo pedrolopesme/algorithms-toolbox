1a8fd8457097d87bb87519eb015e90fcbb1de56b
pedro.mendes <pedro.mendes@corp.globo.com>
2018-07-25 13:49:42 -0300

    classic/bwt/solution_test: add tests to the BWT transformation

--- classic/bwt/solution_test.go
+++ classic/bwt/solution_test.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStringTransformation(test *testing.T) {
+	input := "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES"
+	expectedOutput := "TEXYDST.E.IXIXIXXSSMPPS.B..E.S.EUSFXDIIOIIIT"
+	assert.Equal(test, expectedOutput, Transform(input))
+}
+
+func TestStringTransformationWithNoString(test *testing.T) {
+	input := ""
+	expectedOutput := ""
+	assert.Equal(test, expectedOutput, Transform(input))
+}
