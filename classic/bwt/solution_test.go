5b9fe46d88596e9fcd68f796936c3cc1ed00ff07
pedro.mendes <pedro.mendes@corp.globo.com>
2018-07-25 13:56:48 -0300

    classic/bwt/solution_test: add tests to generate transformations

--- classic/bwt/solution_test.go
+++ classic/bwt/solution_test.go
@@ -5,6 +5,23 @@ import (
 	"testing"
 )
 
+func TestPermutations(test *testing.T) {
+	input := "TOAD"
+	expectedOutput := []string{
+		"TOAD",
+		"OADT",
+		"ADTO",
+		"DTOA",
+	}
+	assert.Equal(test, expectedOutput, generatePermutations(input))
+}
+
+func TestPermutationsWithEmptyString(test *testing.T) {
+	input := ""
+	var expectedOutput []string
+	assert.Equal(test, expectedOutput, generatePermutations(input))
+}
+
 func TestStringTransformation(test *testing.T) {
 	input := "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES"
 	expectedOutput := "TEXYDST.E.IXIXIXXSSMPPS.B..E.S.EUSFXDIIOIIIT"
