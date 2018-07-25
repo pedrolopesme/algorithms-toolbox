36b4f3edaa6fd9ca07d935c66c133b0e1c94c92e
pedro.mendes <pedro.mendes@corp.globo.com>
2018-07-25 13:49:26 -0300

    classic/bwt/solution: implement BWT transformation

--- classic/bwt/solution.go
+++ classic/bwt/solution.go
@@ -1,2 +1,41 @@
 package main
 
+import "sort"
+
+// generatePermutations computes the block
+// permutations needed to the BWT
+// TODO add tests
+func generatePermutations(str string) (permutations []string) {
+	i := 0
+	for i < len(str) {
+		permutations = append(permutations, str[i:]+str[:i])
+		i++
+	}
+	return permutations
+}
+
+// Transform returns a Burrows-Wheeler transformation
+func Transform(str string) (transform string) {
+	permutations := generatePermutations(str)
+	sort.Strings(permutations)
+
+	for index := range permutations {
+		permutation := permutations[index]
+		transform += string(permutation[len(permutation)-1])
+	}
+
+	for index := range permutations {
+		if permutations[index] == str {
+			break
+		}
+	}
+
+	return
+}
+
+// InverseTransform reverses a Burrows-Wheeler transform
+// to the original string
+// TODO implement and add tests
+func InverseTransform() (tranform string) {
+	return
+}
