b9f05715a2f7b4279dc58f9c8e736f02ba5bdf8d
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-02 09:48:22 -0300

    classic/bfs: add graph structure

--- classic/bfs/solution.go
+++ classic/bfs/solution.go
@@ -1,2 +1,24 @@
 package main
 
+type Graph interface {
+	AddEdges(vertex1 int, vertex2 int)
+	GetAdjacencies(vertex int) []int
+}
+
+type BaseGraph struct {
+	nodes map[int][]int
+}
+
+func (g BaseGraph) AddEdges(vertex1 int, vertex2 int) {
+	g.nodes[vertex1] = append(g.nodes[vertex1], vertex2)
+}
+
+func (g BaseGraph) GetAdjacencies(vertex int) []int {
+	return g.nodes[vertex]
+}
+
+func NewGraph() *BaseGraph {
+	return &BaseGraph{
+		nodes: make(map[int][]int),
+	}
+}
