2c6228758d00f5dcc4ffea7b933084c3c9a39d8c
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-03 10:07:26 -0300

    [WIP] classic/bfs/solution: implement solution

--- classic/bfs/solution.go
+++ classic/bfs/solution.go
@@ -1,29 +1,58 @@
 package main
 
 type Graph interface {
+	GetVerticesTotal() int
 	AddEdges(vertex1 int, vertex2 int)
 	GetAdjacencies(vertex int) []int
 }
 
 type BaseGraph struct {
-	nodes map[int][]int
+	// Total of vertices
+	vertices int
+
+	// Edges between vertices, eg. vertex1 -> [vertex2, vertex3, ...]
+	edges map[int][]int
+}
+
+func (g BaseGraph) GetVerticesTotal() int {
+	return g.vertices
 }
 
 func (g BaseGraph) AddEdges(vertex1 int, vertex2 int) {
-	g.nodes[vertex1] = append(g.nodes[vertex1], vertex2)
+	g.edges[vertex1] = append(g.edges[vertex1], vertex2)
 }
 
 func (g BaseGraph) GetAdjacencies(vertex int) []int {
-	return g.nodes[vertex]
+	return g.edges[vertex]
 }
 
-func NewGraph() *BaseGraph {
+func NewGraph(totalOfVertices int) *BaseGraph {
 	return &BaseGraph{
-		nodes: make(map[int][]int),
+		vertices: totalOfVertices,
+		edges: make(map[int][]int),
 	}
 }
 
 // TODO implement and test BFS
-func BFS() {
+func BFS(current int, graph Graph) (path []int) {
+	visited := make([]bool, graph.GetVerticesTotal())
+	queue := make([]int, graph.GetVerticesTotal())
 
+	visited[current] = true
+	queue = append([]int{current}, queue...)
+
+	for len(queue) > 0 {
+		var item int
+		item, queue = queue[0], queue[1:]
+
+		path = append(path, item)
+		for _, adj := range graph.GetAdjacencies(item) {
+			if !visited[adj] {
+				visited[adj] = true
+				queue = append([]int{adj}, queue...)
+			}
+		}
+	}
+
+	return
 }
--- classic/bfs/solution_test.go
+++ classic/bfs/solution_test.go
@@ -5,7 +5,16 @@ import (
 	"testing"
 )
 
-func Test(test *testing.T) {
-	assert.False(test,true)
+func TestBFSStartingFromVertexZero(test *testing.T) {
+	graph := NewGraph(4)
+	graph.AddEdges(0, 1)
+	graph.AddEdges(0, 2)
+	graph.AddEdges(1, 2)
+	graph.AddEdges(2, 0)
+	graph.AddEdges(2, 3)
+	graph.AddEdges(3, 3)
+
+	path := BFS(2, graph)
+	assert.Equal(test,[]int{2, 0, 3, 1}, path)
 }
 
