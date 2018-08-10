572ebc91093d9fa01b71acaccf3aa687ced028aa
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-10 17:20:48 -0300

    classic/insertions-sort: add solution and tests to zero-indexed array

--- classic/insertion-sort/solution.go
+++ classic/insertion-sort/solution.go
@@ -1,2 +1,15 @@
 package main
 
+func InsertionSort(arr []int) (output []int) {
+	output = arr
+	for i := range output {
+		j := i
+		for j > 0 && output[j-1] > output[j] {
+			temp := output[j-1]
+			output[j-1] = output[j]
+			output[j] = temp
+			j--
+		}
+	}
+	return
+}
--- classic/insertion-sort/solution_test.go
+++ classic/insertion-sort/solution_test.go
@@ -1,2 +1,19 @@
 package main
 
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestInsertionSortOnUnSortedArray(test *testing.T) {
+	input := []int{0,6,3,8,4,1,7}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, InsertionSort(input))
+}
+
+func TestInsertionSortOnSortedArray(test *testing.T) {
+	input := []int{0,1,3,4,6,7,8}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, InsertionSort(input))
+}
