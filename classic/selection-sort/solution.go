d2d3161ecb684ac44edb0e11877effa86e89cb7d
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-10 17:56:49 -0300

    classic/selection-sort: add solution and tests

--- classic/selection-sort/solution.go
+++ classic/selection-sort/solution.go
@@ -1,2 +1,19 @@
 package main
 
+func SelectionSort(arr []int) ([]int) {
+	for j := range arr[:len(arr) -1] {
+		min := j
+		for i := j + 1; i < len(arr); i++ {
+			if arr[i] < arr[min] {
+				min = i
+			}
+		}
+
+		if min != j {
+			temp := arr[j]
+			arr[j] = arr[min]
+			arr[min] = temp
+		}
+	}
+	return arr
+}
