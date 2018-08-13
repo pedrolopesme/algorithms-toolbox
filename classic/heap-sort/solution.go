6749c73064d622bdb311fb5d4a2af5256995ecbd
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-13 16:41:05 -0300

    classic/heap-sort: add solution implementation and tests

--- classic/heap-sort/solution.go
+++ classic/heap-sort/solution.go
@@ -1,2 +1,69 @@
 package main
 
+import "math"
+
+// HeapSort rearranges array using heap sort
+func HeapSort(arr []int) ([]int) {
+	arr = heapify(arr)
+	end := len(arr) - 1
+
+	for end > 0 {
+		arr = swap(arr, end, 0)
+		end--
+		arr = siftDown(arr, 0, end)
+	}
+	return arr
+}
+
+// Swaps position i to j in arr array of integers
+func swap(arr []int, i int, j int) []int {
+	temp := arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
+	return arr
+}
+
+// Heapify put elements of 'arr' in heap order, in-place
+func heapify(arr []int) []int {
+	start := iParent(len(arr))
+	end := len(arr) - 1
+
+	for start >= 0 {
+		arr = siftDown(arr, start, end)
+		start--
+	}
+	return arr
+}
+
+func siftDown(arr []int, start int, end int) [] int {
+	root := start
+
+	for iLeftChild(root) <= end {
+		child := iLeftChild(root)
+		swapElement := root
+
+		if arr[swapElement] < arr[child] {
+			swapElement = child
+		}
+
+		if child+1 <= end && arr[swapElement] < arr[child+1] {
+			swapElement = child + 1
+		}
+
+		if swapElement == root {
+			return arr
+		} else {
+			arr = swap(arr, root, swapElement)
+			root = swapElement
+		}
+	}
+	return arr
+}
+
+func iParent(i int) int {
+	return int(math.Floor(float64(i-1) / 2))
+}
+
+func iLeftChild(i int) int {
+	return 2*i + 1
+}
--- classic/heap-sort/solution_test.go
+++ classic/heap-sort/solution_test.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHeapSortOnUnSortedArray(test *testing.T) {
+	input := []int{0,6,3,8,4,1,7}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, HeapSort(input))
+}
+
+func TestHeapSortOnSortedArray(test *testing.T) {
+	input := []int{0,1,3,4,6,7,8}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, HeapSort(input))
+}
