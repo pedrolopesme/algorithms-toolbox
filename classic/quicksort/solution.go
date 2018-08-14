5e330c08ab3f8b4160f91daa1a2b01146031a19d
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-14 10:39:16 -0300

    classic/quicksort: add solution implementation and tests

--- classic/quicksort/solution.go
+++ classic/quicksort/solution.go
@@ -1,2 +1,44 @@
 package main
 
+func QuickSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	start, end := 0, len(arr)-1
+	pivot := generatePivot(arr, start, end)
+	arr[pivot], arr[end] = arr[end], arr[pivot]
+
+	for i := range arr {
+		if arr[i] < arr[end] {
+			arr[start], arr[i] = arr[i], arr[start]
+			start++
+		}
+	}
+
+	arr[start], arr[end] = arr[end], arr[start]
+
+	QuickSort(arr[:start])
+	QuickSort(arr[start+1:])
+	return arr
+}
+
+func generatePivot(arr []int, start int, end int) int {
+	pivot := arr[end]
+	i := start
+	for j := i; j < start-1; j++ {
+		if arr[j] < pivot {
+			arr = swap(arr, i, j)
+			i++
+		}
+	}
+	arr = swap(arr, i, end)
+	return i
+}
+
+func swap(arr []int, i int, j int) []int {
+	temp := arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
+	return arr
+}
--- classic/quicksort/solution_test.go
+++ classic/quicksort/solution_test.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestQuickSortOnUnSortedArray(test *testing.T) {
+	input := []int{0,6,3,8,4,1,7}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, QuickSort(input))
+}
+
+func TestQuickSortOnSortedArray(test *testing.T) {
+	input := []int{0,1,3,4,6,7,8}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, QuickSort(input))
+}
