3895f838b69a390e1af3b5a57197d739b9956744
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-15 14:13:25 -0300

    classic/shellsort : add solution implementation and tests

--- classic/shellsort/solution.go
+++ classic/shellsort/solution.go
@@ -1,2 +1,25 @@
 package main
 
+func ShellSort(arr []int) []int {
+	increment := len(arr) / 2
+	for increment > 0 {
+		for i := increment; i < len(arr); i++ {
+			j := i
+			temp := arr[i]
+
+			for j >= increment && arr[j-increment] > temp {
+				arr[j] = arr[j-increment]
+				j -= increment
+			}
+
+			arr[j] = temp
+		}
+		if increment == 2 {
+			increment = 1
+		} else {
+			increment = int(increment * 5 / 11)
+		}
+	}
+
+	return arr
+}
