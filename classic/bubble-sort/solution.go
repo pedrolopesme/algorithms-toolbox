681c797b714c41c16080f7d6f0ec8323c3ac0058
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-15 09:57:20 -0300

    classic/bubblesort: add solution implementation and tests

--- classic/bubble-sort/solution.go
+++ classic/bubble-sort/solution.go
@@ -1,2 +1,23 @@
 package main
 
+func BubbleSort(arr []int) []int {
+	n := len(arr)
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := 1; i < n; i++ {
+			if arr[i-1] > arr[i] {
+				arr = swap(arr, i-1, i)
+				swapped = true
+			}
+		}
+	}
+	return arr
+}
+
+func swap(arr []int, i int, j int) []int {
+	temp := arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
+	return arr
+}
--- classic/bubble-sort/solution_test.go
+++ classic/bubble-sort/solution_test.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBubbleSortOnUnSortedArray(test *testing.T) {
+	input := []int{0,6,3,8,4,1,7}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, BubbleSort(input))
+}
+
+func TestBubbleSortOnSortedArray(test *testing.T) {
+	input := []int{0,1,3,4,6,7,8}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, BubbleSort(input))
+}
