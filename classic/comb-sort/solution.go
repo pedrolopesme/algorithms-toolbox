32bbd4196c7f1eebb4b4364b10a73f9e9474938e
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-16 11:12:56 -0300

    classic/comb-sort: add solution implementation and tests

--- classic/comb-sort/solution.go
+++ classic/comb-sort/solution.go
@@ -1,1 +1,38 @@
 package main
+
+import "math"
+
+func CombSort(arr []int) []int {
+	gap := len(arr)
+	shrinkRatio := 1.3
+	sorted := false
+
+	for !sorted {
+		gap = int(math.Floor(float64(gap) / shrinkRatio))
+
+		if gap > 1 {
+			sorted = false
+		} else {
+			gap = 1
+			sorted = true
+		}
+
+		i := 0
+		for i+gap < len(arr) {
+			if arr[i] > arr[i+gap] {
+				arr = swap(arr, i, i+gap)
+				sorted = false
+			}
+			i++
+		}
+	}
+
+	return arr
+}
+
+func swap(arr []int, i int, j int) []int {
+	temp := arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
+	return arr
+}
--- classic/comb-sort/solution_test.go
+++ classic/comb-sort/solution_test.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCombSortOnUnSortedArray(test *testing.T) {
+	input := []int{0,6,3,8,4,1,7}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, CombSort(input))
+}
+
+func TestCombSortOnSortedArray(test *testing.T) {
+	input := []int{0,1,3,4,6,7,8}
+	expectedOutput := []int{0,1,3,4,6,7,8}
+	assert.Equal(test, expectedOutput, CombSort(input))
+}
