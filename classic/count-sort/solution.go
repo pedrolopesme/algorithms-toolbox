fb48c1ef29a7b877761fe4b4725256f4531acdd5
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-17 16:11:38 -0300

    classic/count-sort: add solution implementation and tests

--- classic/count-sort/solution.go
+++ classic/count-sort/solution.go
@@ -1,2 +1,24 @@
 package main
 
+func CountSort(arr []rune) []rune {
+	maxKey := 256
+	count := make([]int, maxKey)
+	for _, x := range arr {
+		count[int(x)]++
+	}
+
+	accTotal := 0
+	for i := 1; i < maxKey; i++ {
+		oldCount := count[i]
+		count[i] = accTotal
+		accTotal += oldCount
+	}
+
+	output := make([]rune, maxKey)
+	for _, x := range arr {
+		output[count[int(x)]] = x
+		count[int(x)]++
+	}
+
+	return output[:len(arr)]
+}
--- classic/count-sort/solution_test.go
+++ classic/count-sort/solution_test.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCountSortOnUnSortedArray(test *testing.T) {
+	input := []rune{'b', 'a', 'a', 'y', 'f', 'u'}
+	expectedOutput := []rune{'a', 'a', 'b', 'f', 'u', 'y'}
+	assert.Equal(test, expectedOutput, CountSort(input))
+}
+
+func TestCountSortOnSortedArray(test *testing.T) {
+	input := []rune{'a', 'a', 'b', 'f', 'u', 'y'}
+	expectedOutput := []rune{'a', 'a', 'b', 'f', 'u', 'y'}
+	assert.Equal(test, expectedOutput, CountSort(input))
+}
