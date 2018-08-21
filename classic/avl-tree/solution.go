daa3d9b5e68f73feda293dbd267be79d3854b239
pedro.mendes <pedro.mendes@corp.globo.com>
2018-08-21 10:18:57 -0300

    classic/avl-tree: add structs for Tree and Node and some basic operations (CalcHeight, IsLeaf,RotateRight)

--- classic/avl-tree/solution.go
+++ classic/avl-tree/solution.go
@@ -1,1 +1,58 @@
 package main
+
+// AvlTree defines the basic tree structure
+type AvlTree struct {
+	root   *Node
+	height int
+}
+
+// Node definition used throughout the code.
+type Node struct {
+	id     int
+	height int
+	left   *Node
+	right  *Node
+}
+
+// CalcHeight returns tree's height
+// TODO add tests
+func (tree AvlTree) CalcHeight() int {
+	if tree.root == nil {
+		return 0
+	} else {
+		return tree.root.height
+	}
+}
+
+// IsLeaf checks if a node is leafs
+// TODO add tests
+func (node Node) IsLeaf() bool {
+	return node.height == 0
+}
+
+// max return the maximum value between two integers
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// RotateRight is a helper that rotates node positions
+// in a clockwise direction
+// TODO add tests
+func RotateRight(node *Node) *Node {
+	if node == nil || node.left == nil {
+		return node
+	}
+
+	leftNode := node.left
+	subNode := leftNode.right
+
+	leftNode.right = node
+	node.left = subNode
+
+	node.height = max(node.left.height, node.right.height) + 1
+	node.height = max(leftNode.left.height, leftNode.right.height) + 1
+	return leftNode
+}
