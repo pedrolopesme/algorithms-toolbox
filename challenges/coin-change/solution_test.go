9f3ca6b621599df6340ab1c5476c31123a141412
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-09-09 10:46:46 -0300

    challenges/coin-change: add challenge placeholder

--- challenges/coin-change/solution_test.go
+++ challenges/coin-change/solution_test.go
@@ -1,2 +1,18 @@
-package main
+package coin_change
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCalculateChangeForZeroValue(test *testing.T) {
+	assert.Equal(test, 1, 0)
+}
+
+func TestCalculateChangeForExactCoinMatch(test *testing.T) {
+	assert.Equal(test, 1, 0)
+}
+
+func TestCalculateChangeUsingMultipleCoins(test *testing.T) {
+	assert.Equal(test, 1, 0)
+}
