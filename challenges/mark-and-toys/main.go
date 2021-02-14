1bbdb9c8c5b8d4250b16a75c0fc9db2f9abe74bc
Pedro Mendes <pedrolopesme@gmail.com>
2021-02-14 17:23:56 -0300

    challenge: (WIP) mark and toys

--- challenges/mark-and-toys/main.go
+++ challenges/mark-and-toys/main.go
@@ -9,9 +9,69 @@ import (
 	"strings"
 )
 
+func merge(a1, a2 []int32) []int32 {
+	sorted := []int32{}
+	var i1, i2 int
+
+	if i1 < len(a1) && i2 < len(a2) {
+		if a1[i1] < a2[i2] {
+			sorted = append(sorted, a1[i1])
+			i1++
+		} else {
+			sorted = append(sorted, a2[i2])
+			i2++
+		}
+	}
+
+	sorted = append(sorted, a1[i1:]...)
+	sorted = append(sorted, a2[i2:]...)
+	return sorted
+}
+
+// using mergeSort
+func sortPrices(unsortedPrices []int32) []int32 {
+	if len(unsortedPrices) <= 1 {
+		return unsortedPrices
+	}
+
+	middle := len(unsortedPrices) / 2
+	return merge(sortPrices(unsortedPrices[:middle]), sortPrices(unsortedPrices[middle:]))
+}
+
 // Complete the maximumToys function below.
+// source https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sort
 func maximumToys(prices []int32, k int32) int32 {
-	return int32(0)
+	// it will be easier to compare prices if they are
+	// sorted
+	sortedPrices := sortPrices(prices)
+	var maxItems int32
+
+	for i := 0; i < len(sortedPrices); i++ {
+		// if the current price is bigger than the amount to spend
+		// then thereis not to do
+		if sortedPrices[i] > k {
+			break
+		}
+
+		currQttItems := int32(1)
+		currSumPrice := sortedPrices[i]
+
+		// Check if we are not in the
+		if i < len(sortedPrices)-1 {
+			for j := i + 1; j < len(sortedPrices); j++ {
+				if sortedPrices[j]+currSumPrice <= k {
+					currQttItems++
+					currSumPrice += sortedPrices[j]
+				}
+			}
+		}
+
+		if currQttItems > maxItems {
+			maxItems = currQttItems
+		}
+	}
+
+	return maxItems
 }
 
 func main() {
