ad144fe8617891a728309c5bb767240e1c4fc968
Pedro Mendes <pedrolopesme@gmail.com>
2021-04-20 14:25:32 -0300

    challenge: make fraud activity notifications work faster

--- challenges/fraud-activity-notifications/main.go
+++ challenges/fraud-activity-notifications/main.go
@@ -12,12 +12,69 @@ import (
 
 // Complete the activityNotifications function below.
 // source: https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem
-// TODO it is slow. Make it faster
 func activityNotifications(expenditure []int32, d int32) int32 {
+
+	// lets use count sort to avoid heavy sorting each
+	// iteration on expenditure. First we need to
+	// prepare the beginning of d elements on expenditure
+	expendCountSort := make([]int32, 200) // by definition, max expenditure is 200
+	for i := int32(0); i < d; i++ {
+		expendCountSort[expenditure[i]]++
+	}
+
+	notifications := int32(0)
+	for i := d; i < int32(len(expenditure)); i++ {
+		currentExpense := expenditure[i]
+		median := getMedian(expendCountSort, d)
+
+		if float32(currentExpense) >= 2.0*median {
+			notifications++
+		}
+
+		// update count sort
+		expendCountSort[currentExpense]++
+		expendCountSort[expenditure[i-d]]--
+	}
+
+	return notifications
+}
+
+// this median works adapted to count sort logic
+func getMedian(expenditure []int32, d int32) float32 {
+	if d%2 == 0 {
+		var middle1, middle2 *int
+		count := int32(0)
+
+		for i := 0; i < len(expenditure); i++ {
+			count += expenditure[i]
+			if middle1 == nil && count >= d/2 {
+				middle1 = &i
+			}
+
+			if middle2 == nil && count >= d/2+1 {
+				middle2 = &i
+				break
+			}
+
+		}
+		return float32((*middle1 + *middle2) / 2.0)
+	} else {
+		count := int32(0)
+		for i := 0; i < len(expenditure); i++ {
+			count += expenditure[i]
+			if count > d/2 {
+				return float32(i)
+			}
+		}
+	}
+	return 0.0
+}
+
+func activityNotificationsSlow(expenditure []int32, d int32) int32 {
 	notifications := int32(0)
 	for i := d; i < int32(len(expenditure)); i++ {
 		subset := expenditure[i-d : i]
-		notificationThreshold := 2 * median(subset)
+		notificationThreshold := 2 * medianSlow(subset)
 		dayExpense := float32(expenditure[d])
 		if dayExpense >= notificationThreshold {
 			notifications++
@@ -26,7 +83,7 @@ func activityNotifications(expenditure []int32, d int32) int32 {
 	return notifications
 }
 
-func median(numbers []int32) float32 {
+func medianSlow(numbers []int32) float32 {
 	sort.Slice(numbers, func(i, j int) bool {
 		return numbers[i] < numbers[j]
 	})
