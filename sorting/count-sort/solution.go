e7e3bb974354d646fb048a73157d8c5e0a68eccc
Pedro Mendes <pedrolopesme@gmail.com>
2021-04-20 11:39:59 -0300

    sort: add count sort for integer

--- sorting/count-sort/solution.go
+++ sorting/count-sort/solution.go
@@ -1,6 +1,6 @@
 package main
 
-func CountSort(arr []rune) []rune {
+func CountSortRunes(arr []rune) []rune {
 	maxKey := 256
 	count := make([]int, maxKey)
 	for _, x := range arr {
@@ -21,4 +21,39 @@ func CountSort(arr []rune) []rune {
 	}
 
 	return output[:len(arr)]
+}
+
+func findMax(arr []int) int {
+	var temp int
+
+	temp = arr[0]
+
+	for _, e := range arr {
+		if temp < e {
+			temp = e
+		}
+	}
+
+	return temp
+}
+
+func CountSortInt(arr []int) []int {
+	count := make([]int, findMax(arr))
+
+	for i := 0; i < len(arr); i++ {
+		count[arr[i]-1]++
+	}
+
+	for i := 1; i < len(count); i++ {
+		count[i] += count[i-1]
+	}
+
+	outArr := make([]int, len(arr))
+	for i := 0; i < len(arr); i++ {
+		position := count[arr[i]-1]
+		outArr[position-1] = arr[i]
+		count[arr[i]-1]--
+	}
+
+	return outArr
 }
--- sorting/count-sort/solution_test.go
+++ sorting/count-sort/solution_test.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCountSortOnUnSortedArray(test *testing.T) {
 	input := []rune{'b', 'a', 'a', 'y', 'f', 'u'}
 	expectedOutput := []rune{'a', 'a', 'b', 'f', 'u', 'y'}
-	assert.Equal(test, expectedOutput, CountSort(input))
+	assert.Equal(test, expectedOutput, CountSortRunes(input))
 }
 
 func TestCountSortOnSortedArray(test *testing.T) {
 	input := []rune{'a', 'a', 'b', 'f', 'u', 'y'}
 	expectedOutput := []rune{'a', 'a', 'b', 'f', 'u', 'y'}
-	assert.Equal(test, expectedOutput, CountSort(input))
+	assert.Equal(test, expectedOutput, CountSortRunes(input))
+}
+
+func TestCountSortInt_Case1(test *testing.T) {
+	input := []int{1, 4, 1, 2, 7, 5, 2}
+	expectedOutput := []int{1, 1, 2, 2, 4, 5, 7}
+	assert.Equal(test, expectedOutput, CountSortInt(input))
 }
