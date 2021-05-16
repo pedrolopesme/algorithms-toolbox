63cc3bf3c388a3e2cec307ddb2529c6543f2c292
Pedro Mendes <pedrolopesme@gmail.com>
2021-05-16 16:04:45 -0300

    search: add interative binary search

--- search/binay-search/main.go
+++ search/binay-search/main.go
@@ -15,3 +15,21 @@ func binarySearchRecursive(arr []int, val, from, to int) int {
 		return binarySearchRecursive(arr, val, mid+1, to)
 	}
 }
+
+func binarySearchInterative(arr []int, val int) int {
+	from := 0
+	to := len(arr) - 1
+
+	for from <= to {
+		mid := (from + to) / 2
+
+		if arr[mid] == val {
+			return mid
+		} else if val < arr[mid] {
+			to = mid - 1
+		} else {
+			from = mid + 1
+		}
+	}
+	return -1
+}
--- search/binay-search/main_test.go
+++ search/binay-search/main_test.go
@@ -15,3 +15,13 @@ func TestBSRecursive_Case2(t *testing.T) {
 	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	assert.Equal(t, -1, binarySearchRecursive(arr, 10, 0, len(arr)-1))
 }
+
+func TestBSInterative_Case1(t *testing.T) {
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	assert.Equal(t, 6, binarySearchInterative(arr, 6))
+}
+
+func TestBSInterative_Case2(t *testing.T) {
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	assert.Equal(t, -1, binarySearchInterative(arr, 10))
+}
