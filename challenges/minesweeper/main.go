7eb4c2a47c24ebcf554dbfb6eda3aece7844a9b6
Pedro Mendes <pedrolopesme@gmail.com>
2021-05-21 17:56:40 -0300

    challenges: minesweeper - printing column

--- challenges/minesweeper/main.go
+++ challenges/minesweeper/main.go
@@ -24,13 +24,24 @@ type Board struct {
 	cells [][]Cell
 }
 
+func (b *Board) PrintColumnHeader() {
+	for column := 0; column < len(b.cells[0]); column++ {
+		fmt.Printf("%d ", column)
+	}
+	fmt.Println()
+}
+
 func (b *Board) Print() {
 	columnsSize := len(b.cells)
 	rowSize := len(b.cells[0])
 
-	// walk throught the entire matrix to print a cells
+	// walk through the entire matrix to print cells
 	for row := 0; row < rowSize; row++ {
 		for column := 0; column < columnsSize; column++ {
+			if row == 0 && column == 0 {
+				b.PrintColumnHeader()
+			}
+
 			b.cells[row][column].Print()
 		}
 		fmt.Println()
@@ -42,6 +53,7 @@ func New(columnsSize, rowSize int) *Board {
 	board.cells = make([][]Cell, rowSize)
 
 	// initializing all cells
+	// TODO is it really necessary?
 	for row := 0; row < rowSize; row++ {
 		board.cells[row] = make([]Cell, columnsSize)
 		for column := 0; column < columnsSize; column++ {
